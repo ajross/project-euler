3c3e55d7c120a350f7c45181a2378def476ab735
Alasdair Ross <Alasdair.Ross@gmail.com>
2013-10-09 22:32:00 -0400

    Problem 6 in C.  Inefficient calculation of sum of squares

---
+++ c/problem_6.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+
+int main (int argc, char **argv) {
+    int limit = 100;
+    int sq_sum = (limit * (limit + 1)) / 2;
+    sq_sum *= sq_sum;
+
+    int sum_sq = 0;
+    int i;
+
+    for (i = 1; i <= limit; i++) {
+        sum_sq += i * i;
+    }
+
+    printf("Sum of squares from 1 to %d is: %d\n", limit, sum_sq);
+    printf("Square of sums from 1 to %d is: %d\n", limit, sq_sum);
+    printf("Difference is: %d\n", sum_sq - sq_sum);
+
+    return 0;
+}
