9de1d85f3967cb118c26c35ddeae1f4057cbed65
Alasdair Ross <Alasdair.Ross@gmail.com>
2013-10-10 01:49:38 -0400

    Quicker, but may be buggy...
    We don't store or calculate any even numbers.
    The count of prime numbers below 10 billion doesn't match what it says online.
    I need to verify this...and probably write a unit test...

--- c/problem_7.c
+++ c/problem_7.c
@@ -1,36 +1,37 @@
 #include "stdlib.h"
 #include "stdio.h"
 
-#define MEMSIZE 1000000000
+#define MEMSIZE 10000000000
 
 int main(int argc, char **argv) {
 
     char *mem = NULL;
-    unsigned long count = 0;
+    unsigned long count = 1;
 
     mem = calloc(MEMSIZE, sizeof(char));
 
-    unsigned long i, j;
-    for (i = 4; i < MEMSIZE; i+=2) {
-        mem[i] = 1;
-    }
+    unsigned long i, j, index;
 
-    for (i = 3; i < MEMSIZE; i+=2) {
-        for (j = i * i; j < MEMSIZE; j += i) {
-            mem[j] = 1;
+    for(i = 3; i < MEMSIZE; i += 2){
+        for (j = i * i; j < MEMSIZE; j += (i*2)) {
+            index = (j/2);
+            mem[index] = 1;
+//            printf("i = %lu, j = %lu, index = %lu\n", i, j, index);
         }
     }
 
-    for (i = 2; i < MEMSIZE; i++) {
+
+    for (i = 1; i < MEMSIZE; i++) {
         if(mem[i] == 0) {
+//            printf("%lu\n", (unsigned long)((i*2) + 1));
             count ++;
             if (count == 10001) {
-                printf("The 10,001st prime number is: %lu\n", i);
+                printf("The 10,001st prime number is: %lu\n", (unsigned long)((i*2) + 1));
             }
         }
     }
 
-    printf("%lu primes below %lu\n", count, (unsigned long)MEMSIZE);
+    printf("%lu primes below %lu\n", count, (unsigned long)MEMSIZE * 2);
 
     free(mem);
 
